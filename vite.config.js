774d466abb8b8212b4ff084f6f458bed57195971
FLITIMouad <asus16mouad@gmail.com>
2025-02-20 01:54:51 +0100

    change config of vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { copyFileSync } from 'fs';
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
 })
